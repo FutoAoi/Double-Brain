c8575b53e2c03a8cca0eab3a744ed5fc56eaef04
ToTuKa <Kt08007921@gmail.com>
2026-01-20 16:31:07 +0900

    [update] シーン移行時、キーボードに対応できるように(キー固定)

--- Assets/Kotarou/OpenCanvasUI.cs
+++ Assets/Kotarou/OpenCanvasUI.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-/// <summary>
-/// オブジェクト、ボタンクリックでキャンバス表示
-/// スペースによる表示、エスケープによる非表示
-/// </summary>
+/*
+ * クリックとキーボード操作で動くように
+ * キーボードの場合SPACEで開き、ESCAPEで閉じる
+ */
 
 public class OpenCanvasUI : MonoBehaviour
 {
-    [SerializeField] GameObject panel;
+    [SerializeField] GameObject panel; //表示、非表示させるパネル(オブジェクト)
 
     void Start()
     {
@@ -18,22 +18,25 @@ public class OpenCanvasUI : MonoBehaviour
     //特定キーが押されたらパネルを表示、非表示
     void Update()
     {
+        //スペース
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             ShowPanel();
         }
-
+        //エスケープ
         if (Keyboard.current.escapeKey.wasPressedThisFrame)
         {
             HidePanel();
         }
     }
 
+    //開く
     public void ShowPanel()
     {
         panel.SetActive(true);
     }
 
+    //閉じる
     public void HidePanel()
     {
         panel.SetActive(false);
--- Assets/Kotarou/SceneChange.cs
+++ Assets/Kotarou/SceneChange.cs
@@ -1,6 +1,13 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+
+/*
+ * シーンネームを入れてシーン移行できるように
+ * UIのボタンで動く
+ * S、Q、Eキーでそれぞれシーン移行
+ */
 
 public class SceneChange : MonoBehaviour
 {
@@ -8,8 +15,28 @@ public class SceneChange : MonoBehaviour
     [SerializeField] string _nextSceneName;
     [SerializeField] string _backSceneName;
 
+    //特定キーが押されたら実行
+    void Update()
+    {
+        //S
+        if (Keyboard.current.sKey.wasPressedThisFrame)
+        {
+            NextStage();
+        }
+        //Q
+        else if (Keyboard.current.qKey.wasPressedThisFrame)
+        {
+            BackScene();
+        }
+        //E
+        else if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            EndGame();
+        }
+    }
+
     //次シーン移行
-    public void Next()
+    public void NextStage()
     {
         if (!string.IsNullOrEmpty(_nextSceneName))
         {
@@ -18,7 +45,7 @@ public class SceneChange : MonoBehaviour
     }
 
     //前シーン移行
-    public void Back()
+    public void BackScene()
     {
         if (!string.IsNullOrEmpty(_backSceneName))
         {
@@ -26,8 +53,8 @@ public class SceneChange : MonoBehaviour
         }
     }
 
-    //ゲーム終了
-    public void End()
+    //ゲーム終了(強制終了)
+    public void EndGame()
     {
         Application.Quit();
 
